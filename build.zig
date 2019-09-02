bd627ed411e67df587f5b6ea06317ee47e6feb33
Marco Jonkers <contact@marcojonkers.com>
2019-09-02 22:55:35 +0200

    Use zig fmt, remove old example code

--- build.zig
+++ build.zig
@@ -2,44 +2,36 @@ const std = @import("std");
 const os = std.os;
 const Builder = std.build.Builder;
 
-pub fn build(b: *Builder) void
-{
-  const mode = b.standardReleaseOptions();
-  const exe = b.addExecutable("hello_windows", "main.zig");
-  exe.setBuildMode(mode);
+pub fn build(b: *Builder) void {
+    std.debug.warn("Hello build!\n");
 
-  exe.addIncludeDir("SDL2-2.0.9/include");
+    const mode = b.standardReleaseOptions();
+    const exe = b.addExecutable("hello_windows", "main.zig");
+    exe.setBuildMode(mode);
 
-  exe.linkSystemLibrary("c");
-  b.addNativeSystemLibPath("SDL2-2.0.9/lib/x64");
-  //exe.linkSystemLibrary("SDL2main.lib");
-  exe.linkSystemLibrary("SDL2.lib");
+    exe.addIncludeDir("SDL2-2.0.9/include");
 
-  var buildDirExists = true;
-  os.makeDir("bin") catch |err|
-  {
-    if (err != error.PathAlreadyExists)
-    {
-      buildDirExists = false;
-      std.debug.warn("Failed to create output directory: {}\n", @errorName(err));
+    exe.linkSystemLibrary("c");
+    b.addNativeSystemLibPath("SDL2-2.0.9/lib/x64");
+    exe.linkSystemLibrary("SDL2.lib");
+
+    var buildDirExists = true;
+    os.makeDir("bin") catch |err| {
+        if (err != error.PathAlreadyExists) {
+            buildDirExists = false;
+            std.debug.warn("Failed to create output directory: {}\n", @errorName(err));
+        }
+    };
+
+    if (buildDirExists) {
+        exe.setOutputDir("./bin");
+
+        const run_cmd = exe.run();
+
+        const run_step = b.step("run", "Run the app");
+        run_step.dependOn(&run_cmd.step);
+
+        b.default_step.dependOn(&exe.step);
+        b.installArtifact(exe);
     }
-  };
-
-  if (buildDirExists)
-  {
-    exe.setOutputDir("./bin");
- 
-    const run_cmd = exe.run();
- 
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
- 
-    b.default_step.dependOn(&exe.step);
-    b.installArtifact(exe);
-
-//    b.copyFile("SDL2-2.0.9\\lib\\x64\\SDL2.dll", "C:\\Repositories\\zig_test\\bin\\") catch |err|
-//    {
-//      std.debug.warn("asdadasd: {}\n", @errorName(err));
-//    };
-  }
 }
--- main.zig
+++ main.zig
@@ -1,20 +1,6 @@
-//use @import("std").os.windows;
-//
-//// The extern specifier is used to declare a function that will be resolved
-//// at link time, when linking statically, or at runtime, when linking
-//// dynamically.
-//// The stdcallcc specifier changes the calling convention of the function.
-//extern "user32" stdcallcc fn MessageBoxA(hWnd: ?HANDLE, lpText: ?LPCTSTR, lpCaption: ?LPCTSTR, uType: UINT) c_int;
-//
-//export fn WinMain(hInstance: HINSTANCE, hPrevInstance: HINSTANCE, lpCmdLine: PWSTR, nCmdShow: INT) INT
-//{
-//    _ = MessageBoxA(null, c"hello", c"title", 0);
-//    return 0;
-//}
-
 const c = @cImport({
-  @cInclude("SDL.h");
-  });
+    @cInclude("SDL.h");
+});
 const assert = @import("std").debug.assert;
 
 // See https://github.com/zig-lang/zig/issues/565
@@ -34,82 +20,75 @@ extern fn SDL_PollEvent(event: *c.SDL_Event) c_int;
 // and to debuggers.
 // SDL_rwops.h:#define SDL_RWclose(ctx)        (ctx)->close(ctx)
 inline fn SDL_RWclose(ctx: [*]c.SDL_RWops) c_int {
-  return ctx[0].close.?(ctx);
+    return ctx[0].close.?(ctx);
 }
 
-pub fn main() !void
-{
-  if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0)
-  {
-    c.SDL_Log(c"Unable to initialize SDL: %s", c.SDL_GetError());
-    return error.SDLInitializationFailed;
-  }
-  defer c.SDL_Quit();
-
-  const screen = c.SDL_CreateWindow(c"My Game Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 300, 73, c.SDL_WINDOW_OPENGL) orelse
-  {
-    c.SDL_Log(c"Unable to create window: %s", c.SDL_GetError());
-    return error.SDLInitializationFailed;
-  };
-  defer c.SDL_DestroyWindow(screen);
-
-  const renderer = c.SDL_CreateRenderer(screen, -1, 0) orelse
-  {
-    c.SDL_Log(c"Unable to create renderer: %s", c.SDL_GetError());
-    return error.SDLInitializationFailed;
-  };
-  defer c.SDL_DestroyRenderer(renderer);
-
-  const zig_bmp = @embedFile("zig.bmp");
-  
-  const optional0: ?[*]c.SDL_RWops = c.SDL_RWFromConstMem(
-    @ptrCast(*const c_void, &zig_bmp[0]),
-    @intCast(c_int, zig_bmp.len),
-    );
-  const rw = optional0 orelse
-  {
-    c.SDL_Log(c"Unable to get RWFromConstMem: %s", c.SDL_GetError());
-    return error.SDLInitializationFailed;
-  };
-  defer assert(SDL_RWclose(rw) == 0);
-
-  const optional1: ?*c.SDL_Surface = c.SDL_LoadBMP_RW(rw, 0);
-  const zig_surface = optional1 orelse
-  {
-    c.SDL_Log(c"Unable to load bmp: %s", c.SDL_GetError());
-    return error.SDLInitializationFailed;
-  };
-  defer c.SDL_FreeSurface(zig_surface);
-
-  const zig_texture = c.SDL_CreateTextureFromSurface(renderer, zig_surface) orelse
-  {
-    c.SDL_Log(c"Unable to create texture from surface: %s", c.SDL_GetError());
-    return error.SDLInitializationFailed;
-  };
-  defer c.SDL_DestroyTexture(zig_texture);
-
-  var quit = false;
-  while (!quit)
-  {
-    var event: c.SDL_Event = undefined;
-    while (SDL_PollEvent(&event) != 0)
-    {
-      switch (event.@"type")
-      {
-        c.SDL_QUIT =>
-        {
-          quit = true;
-        },
-          else =>
-          {},
-      }
+pub fn main() !void {
+    if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
+        c.SDL_Log(c"Unable to initialize SDL: %s", c.SDL_GetError());
+        return error.SDLInitializationFailed;
     }
+    defer c.SDL_Quit();
 
-    _ = c.SDL_RenderClear(renderer);
-    const n: ?[*]c.SDL_Rect = null;
-    _ = c.SDL_RenderCopy(renderer, zig_texture, n, n);
-    c.SDL_RenderPresent(renderer);
+    const screen = c.SDL_CreateWindow(c"My Game Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, c.SDL_WINDOW_OPENGL) orelse
+        {
+        c.SDL_Log(c"Unable to create window: %s", c.SDL_GetError());
+        return error.SDLInitializationFailed;
+    };
+    defer c.SDL_DestroyWindow(screen);
 
-    c.SDL_Delay(17);
-  }
+    const renderer = c.SDL_CreateRenderer(screen, -1, 0) orelse
+        {
+        c.SDL_Log(c"Unable to create renderer: %s", c.SDL_GetError());
+        return error.SDLInitializationFailed;
+    };
+    defer c.SDL_DestroyRenderer(renderer);
+
+    const zig_bmp = @embedFile("zig.bmp");
+
+    const optional0: ?[*]c.SDL_RWops = c.SDL_RWFromConstMem(
+        @ptrCast(*const c_void, &zig_bmp[0]),
+        @intCast(c_int, zig_bmp.len),
+    );
+    const rw = optional0 orelse
+        {
+        c.SDL_Log(c"Unable to get RWFromConstMem: %s", c.SDL_GetError());
+        return error.SDLInitializationFailed;
+    };
+    defer assert(SDL_RWclose(rw) == 0);
+
+    const optional1: ?*c.SDL_Surface = c.SDL_LoadBMP_RW(rw, 0);
+    const zig_surface = optional1 orelse
+        {
+        c.SDL_Log(c"Unable to load bmp: %s", c.SDL_GetError());
+        return error.SDLInitializationFailed;
+    };
+    defer c.SDL_FreeSurface(zig_surface);
+
+    const zig_texture = c.SDL_CreateTextureFromSurface(renderer, zig_surface) orelse
+        {
+        c.SDL_Log(c"Unable to create texture from surface: %s", c.SDL_GetError());
+        return error.SDLInitializationFailed;
+    };
+    defer c.SDL_DestroyTexture(zig_texture);
+
+    var quit = false;
+    while (!quit) {
+        var event: c.SDL_Event = undefined;
+        while (SDL_PollEvent(&event) != 0) {
+            switch (event.@"type") {
+                c.SDL_QUIT => {
+                    quit = true;
+                },
+                else => {},
+            }
+        }
+
+        _ = c.SDL_RenderClear(renderer);
+        const n: ?[*]c.SDL_Rect = null;
+        _ = c.SDL_RenderCopy(renderer, zig_texture, n, n);
+        c.SDL_RenderPresent(renderer);
+
+        c.SDL_Delay(17);
+    }
 }
